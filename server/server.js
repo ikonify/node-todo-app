14ba1b887127a84c90262b123c50fb3c61f56762
Ikonify <hemisalo@gmail.com>
2017-10-27 11:42:08 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,7 +30,31 @@ app.get('/todos', (req, res) => {
     });
 });
 
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        } 
+        
+        res.status(200).send({todo});
+
+    }).catch((err) => {
+        res.status(400).send();
+        });
+
+    // findById
+    // success
+        // if todo - send it back
+        // if no todo - send back 404 with empty body
+    // error - send 400
+});
 
 app.listen(3000, () => {
     console.log(`Server running port 3000`);
